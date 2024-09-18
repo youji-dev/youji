a65e030a45efdfb4f2d61d1471165997022824dd
Rico <rico_zange@gmx.de>
2024-09-18 11:32:40 +0200

    add comment controller

---
+++ backend/WebApi/Controllers/TicketCommentController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersistenceLayer.DataAccess.Entities;
+using PersistenceLayer.DataAccess.Repositories;
+
+namespace Application.WebApi.Controllers
+{
+    /// <summary>
+    /// Controller that provides endpoints to manage ticket comments requests.
+    /// </summary>
+    /// <param name="commentRepo">Instance of <see cref="TicketComment"/></param>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketCommentController(TicketCommentRepository commentRepo) : ControllerBase
+    {
+        /// <summary>
+        /// Deletes the comment with the specific id.
+        /// </summary>
+        /// <param name="id">The specific id of the comment.</param>
+        /// <returns>An <see cref="ObjectResult"/> with a result message.</returns>
+        [HttpDelete]
+        public async Task<ActionResult> Delete(string id)
+        {
+            var comment = await commentRepo.GetAsync(new Guid(id));
+
+            if (comment == null)
+            {
+                return this.NotFound();
+            }
+
+            await commentRepo.DeleteAsync(comment);
+
+            return this.Ok($"Der Kommentar mit der ID '{id}' wurde gelöscht.");
+        }
+    }
+}

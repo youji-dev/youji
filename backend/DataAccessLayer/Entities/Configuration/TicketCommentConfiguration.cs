53f961fba8269e6dbf2ea92ecc44ee7386f17760
yst_ <111455503+m-yst-ery@users.noreply.github.com>
2024-09-18 12:29:32 +0200

    fix db initialize migration and added uuid generation (#40)

---
+++ backend/DataAccessLayer/Entities/Configuration/TicketCommentConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace PersistenceLayer.DataAccess.Entities.Configuration
+{
+    /// <summary>
+    /// Entity configuration for <see cref="TicketComment"/>
+    /// </summary>
+    internal class TicketCommentConfiguration : IEntityTypeConfiguration<TicketComment>
+    {
+        /// <inheritdoc />
+        public void Configure(EntityTypeBuilder<TicketComment> builder)
+        {
+            builder.Property(e => e.Id)
+                .HasColumnType("uuid")
+                .HasDefaultValueSql("uuid_generate_v4()")
+                .IsRequired();
+        }
+    }
+}

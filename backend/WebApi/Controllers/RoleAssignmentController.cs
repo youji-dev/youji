05bd9a971a842b97bf6c16a71efcb514a982166b
Rico <rico_zange@gmx.de>
2024-09-18 12:00:56 +0200

    add role assignment controller

---
+++ backend/WebApi/Controllers/RoleAssignmentController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersistenceLayer.DataAccess.Entities;
+using PersistenceLayer.DataAccess.Repositories;
+
+namespace Application.WebApi.Controllers
+{
+    /// <summary>
+    /// Controller that provides endpoints to manage role assignment requests.
+    /// </summary>
+    /// <param name="roleAssignmentRepo">Instance of <see cref="RoleAssignmentRepository"/></param>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleAssignmentController(RoleAssignmentRepository roleAssignmentRepo) : ControllerBase
+    {
+        /// <summary>
+        /// Gets all role assignments.
+        /// </summary>
+        /// <returns>An <see cref="ObjectResult"/> with all role assignments.</returns>
+        [HttpGet]
+        public ActionResult<RoleAssignment[]> Get()
+        {
+            return this.Ok(roleAssignmentRepo.GetAllAsync(roleAssignment => roleAssignment.Count() > 0));
+        }
+
+        /// <summary>
+        /// Adds a new role assignment entity.
+        /// </summary>
+        /// <param name="roleAssignment">Instance of <see cref="RoleAssignment"/></param>
+        /// <returns>An <see cref="ObjectResult"/> with the added role assignment entity.</returns>
+        [HttpPost]
+        public async Task<ActionResult<RoleAssignment[]>> Post(RoleAssignment roleAssignment)
+        {
+            await roleAssignmentRepo.AddAsync(roleAssignment);
+            return this.Ok(roleAssignment);
+        }
+
+        /// <summary>
+        /// Updates the specific role assignment.
+        /// </summary>
+        /// <param name="id">Instance of <see cref="RoleAssignment"/>.</param>
+        /// <returns>An <see cref="ObjectResult"/> with the updated role assignment.</returns>
+        [HttpDelete]
+        public async Task<ActionResult<RoleAssignment[]>> Delete(string id)
+        {
+            var deleteRoleAssignment = await roleAssignmentRepo.GetAsync(new Guid(id));
+
+            if (deleteRoleAssignment is null)
+            {
+                return this.BadRequest($"Eine Priorität mit der ID '{id}' existiert nicht.");
+            }
+
+            await roleAssignmentRepo.DeleteAsync(deleteRoleAssignment);
+
+            return this.Ok($"Die Priorität mit der ID '{id}' wurde gelöscht.");
+        }
+    }
+}
--- backend/WebApi/Controllers/TicketController.cs
+++ backend/WebApi/Controllers/TicketController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using PersistenceLayer.DataAccess.Entities;
 using PersistenceLayer.DataAccess.Repositories;
 

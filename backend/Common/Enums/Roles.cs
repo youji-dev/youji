1d93ec864184c63d9f421acae48eaa0115d7d4d9
Dustin <private@dustin-dm.de>
2024-09-20 10:14:50 +0200

    codereview changes

--- backend/Common/Enums/Roles.cs
+++ backend/Common/Enums/Roles.cs
@@ -7,18 +7,23 @@ namespace Common.Enums
     public enum Roles
     {
         /// <summary>
+        /// User has no assigned role
+        /// </summary>
+        None = 0,
+
+        /// <summary>
         /// User can create and edit his own tickets and see other tickets
         /// </summary>
-        Teacher = 0,
+        Teacher = 1,
 
         /// <summary>
         /// User can manage all tickets
         /// </summary>
-        FacilityManager = 1,
+        FacilityManager = 2,
 
         /// <summary>
         /// User can manage all Tickets and administrate the system
         /// </summary>
-        Admin = 2,
+        Admin = 4,
     }
 }
--- backend/WebApi/Controllers/AuthController.cs
+++ backend/WebApi/Controllers/AuthController.cs
@@ -19,8 +19,7 @@ namespace Application.WebApi.Controllers
         /// <param name="loginRequestDto">Credentials provided by user</param>
         /// <param name="authenticationService">Instance of <see cref="AuthenticationService"/></param>
         /// <returns>A token pair if authentication succeeds</returns>
-        [ProducesResponseType(type: typeof(LoginResponseDto), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         [HttpPost("login")]
         public async Task<ActionResult<LoginResponseDto>> Login(
             [FromBody] LoginRequestDto loginRequestDto,
@@ -53,8 +52,7 @@ namespace Application.WebApi.Controllers
         /// <param name="refreshRequestDto">Refresh token provided by client</param>
         /// <param name="authenticationService">Instance of <see cref="AuthenticationService"/></param>
         /// <returns>A token pair if refresh token validation succeeds</returns>
-        [ProducesResponseType(type: typeof(LoginResponseDto), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         [HttpPost("refresh")]
         public async Task<ActionResult<LoginResponseDto>> Refresh(
             [FromBody] RefreshRequestDto refreshRequestDto,
@@ -82,9 +80,7 @@ namespace Application.WebApi.Controllers
         /// <summary>
         /// Route used to verify if the accessToken is valid
         /// </summary>
-        /// <returns>20</returns>
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        /// <returns>204 no content</returns>
         [Authorize]
         [HttpGet("verify-token")]
         public ActionResult VerifyToken()

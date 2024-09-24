7d704439ef582665d39ad8cda7003bf11c3619aa
Rico <rico_zange@gmx.de>
2024-09-24 11:22:55 +0200

    add state dto

---
+++ backend/Common/Contracts/StateDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace Common.Contracts
+{
+    /// <summary>
+    /// Represents a state data transfer object.
+    /// </summary>
+    public class StateDTO
+    {
+        /// <summary>
+        /// The name of the state.
+        /// </summary>
+        public required string Name { get; set; }
+
+        /// <summary>
+        /// The color of the state.
+        /// </summary>
+        public required string Color { get; set; }
+    }
+}

21db0c9425bc7ff5873182faa711fc34d72c2fce
Dustin <private@dustin-dm.de>
2024-09-24 10:36:20 +0200

    revert dev changes

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -3,21 +3,7 @@
     <!-- Index page. Still undecided what the content of this should be or if we even want something like this. -->
     <!-- Could contain things like statistics of all tickets or a list of tickets assigned to the authenticated user (maybe ordered by priority) -->
     <!-- If we decide to leave not include this, the index page should be /tickets/ (tickets/index.vue) -->
-    <component :is="Sidebar"></component>
-    <el-button class="m-5 flex justify-center" @click="makeRequest">Make Request</el-button>
-
 </template>
 
 <script lang="ts" setup>
-import Sidebar from '~/components/sidebar.vue';
-
-const makeRequest = () => {
-    useAuthStore().checkIfTokenIsValid();
-} 
-
-definePageMeta({
-    middleware: "auth"
-})
 </script>
-
-<style></style>

45c5032c18e7bf711d77698b931da65e1281a7b6
Erik <erik.leimert@web.de>
2024-09-25 12:30:12 +0200

    started mailing job

---
+++ backend/WebApi/Jobs/Mailing/TicketChangedJob.cs
@@ -0,0 +1,14 @@
+﻿using Quartz;
+
+namespace Application.WebApi.Jobs.Mailing
+{
+    public class TicketChangedJob : IJob
+    {
+
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

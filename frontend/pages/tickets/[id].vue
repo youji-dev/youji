595fe86af45b1d2986091a917e8da06e836a0b23
Dustdev_DM <public@dustin-dm.de>
2024-10-01 22:59:29 +0200

    add responsive desktop css

--- frontend/pages/tickets/[id].vue
+++ frontend/pages/tickets/[id].vue
@@ -1,30 +1,31 @@
 <template>
   <!-- Route: /tickets/[ticketId] -->
   <!-- Page for detail view of a ticket -->
-  <div class="h-[100vh] p-6 mt-8 lg:mt-0" style="height: calc(100vh - 72px)" :style="{ width: width }">
+  <div class="grid grid-cols-1 gap-3 auto-rows-min lg:grid-cols-10 px-6 py-1 mt-12 lg:mt-0" :style="{ width: width }">
     <!-- header -->
-    <div class="flex my-2 sticky">
-      <!-- backbutton -->
-      <el-button class="text-sm flex-none" link @click="router.back()" :icon="ArrowLeft">{{ $t("back") }}</el-button>
-      <!-- separator -->
-      <el-divider class="flex-none self-center" direction="vertical" />
-      <!-- Title -->
-      <el-text class="font-semibold flex-auto truncate" size="large">TicketName</el-text>
-      <!-- Edit Button -->
-      <el-button class="text-sm flex-none justify-self-end drop-shadow-xl" type="primary" :icon="EditPen"
-        @click="toggleEditMode">{{
-          $t("edit") }}</el-button>
+    <div class="flex justify-between lg:col-span-full lg:row-start-1 lg:row-end-2">
+      <div>
+        <!-- backbutton -->
+        <el-button class="text-sm" link @click="router.back()" :icon="ArrowLeft">{{ $t("back") }}</el-button>
+        <!-- separator -->
+        <el-divider class="self-center" direction="vertical" />
+        <!-- Title -->
+        <el-text class="font-semibold truncate" size="large">TicketName</el-text>
+      </div>
+      <div>
+        <!-- Edit Button -->
+        <el-button class="text-sm drop-shadow-xl" type="primary" :icon="EditPen" @click="toggleEditMode">{{ $t("edit") }}</el-button>
+      </div>
     </div>
     <!-- Dropdown Group -->
-    <div class="grid gap-3 grid-cols-2 grid-rows-2">
+    <div class="grid lg:grid-cols-2 lg:grid-rows-4 gap-1 self-center | lg:col-start-7 lg:col-end-11 lg:row-start-2 lg:row-end-3">
       <!-- State dropdown -->
       <div>
         <el-text>{{ $t("state") }}</el-text>
-        <el-select v-model="form.state" value-key="id" class="drop-shadow-xl" :placeholder='$t("select")'>
+        <el-select v-model="form.state" value-key="id" class="drop-shadow-xl" :placeholder="$t('select')">
           <el-option v-for="state in stateOptions" :key="state.id" :label="state.name" :value="state">
             <el-tag :color="state.color">
-              <el-text :style='{ color: contrastColor({ bgColor: state.color }) }'> {{ state.name
-                }}</el-text>
+              <el-text :style="{ color: contrastColor({ bgColor: state.color }) }"> {{ state.name }}</el-text>
             </el-tag>
           </el-option>
           <template #label>
@@ -38,47 +39,44 @@
       <!-- Priority dropdown -->
       <div>
         <el-text>{{ $t("priority") }}</el-text>
-        <el-select v-model="form.priority" value-key="value" class="drop-shadow-xl" :placeholder='$t("select")'>
-          <el-option v-for="priority in priorityOptions" :key="priority.value" :label="priority.name"
-            :value="priority" />
+        <el-select v-model="form.priority" value-key="value" class="drop-shadow-xl" :placeholder="$t('select')">
+          <el-option v-for="priority in priorityOptions" :key="priority.value" :label="priority.name" :value="priority" />
+        </el-select>
+      </div>
+      <!-- Building dropdown -->
+      <div class="lg:col-span-full">
+        <el-text>{{ $t("building") }}</el-text>
+        <el-select v-model="form.building" class="drop-shadow-xl" value-key="id" :clearable="true" :placeholder="$t('select')">
+          <el-option v-for="building in buildingOptions" :key="building.id" :label="building.name" :value="building" />
         </el-select>
       </div>
       <!-- Room textfield -->
-      <div>
+      <div class="lg:col-span-full">
         <el-text>{{ $t("room") }}</el-text>
-        <el-input v-model="form.room" class="drop-shadow-xl" :placeholder='$t("enter")' />
-      </div>
-      <!-- Building dropdown -->
-      <div>
-        <el-text>{{ $t("building") }}</el-text>
-        <el-select v-model="form.building" class="drop-shadow-xl" value-key="id" :clearable="true"
-          :placeholder='$t("select")'>
-          <el-option v-for=" building in buildingOptions" :key="building.id" :label="building.name" :value="building" />
-        </el-select>
+        <el-input v-model="form.room" class="drop-shadow-xl" :placeholder="$t('enter')" />
       </div>
       <!-- Object -->
-      <div>
+      <div class="lg:col-span-full">
         <el-text>{{ $t("object") }}</el-text>
-        <el-input v-model="form.object" class="drop-shadow-xl" :placeholder='$t("enter")' />
+        <el-input v-model="form.object" class="drop-shadow-xl" :placeholder="$t('enter')" />
       </div>
     </div>
 
     <!-- Description -->
-    <div class="mt-6">
+    <div class="lg:col-start-1 lg:col-end-7 lg:row-start-2 lg:row-end-3">
       <el-text>{{ $t("description") }}</el-text>
-      <el-input v-model="form.description" type="textarea" class="drop-shadow-xl" resize="vertical" :rows="10"
-        :placeholder='$t("enter")' />
+      <el-input v-model="form.description" type="textarea" class="drop-shadow-xl max-h-full" rows="15" resize="vertical" :placeholder="$t('enter')" />
     </div>
 
     <!-- meta data -->
-    <div class="flex mt-2 justify-around">
+    <div class="flex justify-around self-start lg:block lg:text-right lg:col-start-7 lg:col-end-11 lg:row-start-4 lg:row-end-5">
       <el-text class="w-1/2 truncate text-center">{{ $t("createdBy") }}: {{ form.author }}</el-text>
-      <el-text class="w-1/2 text-center	">{{ $t("createdOn") }}: {{ form.createdAt }}</el-text>
+      <br />
+      <el-text class="w-1/2 text-center">{{ $t("createdOn") }}: {{ form.createdAt }}</el-text>
     </div>
 
     <!-- files -->
-    <div
-      class="mt-2 px-4 pb-4 pt-2 drop-shadow-xl bg-white dark:bg-black dark:border-[#4c4d4f] dark:border-[1px] rounded-[4px]">
+    <el-card class="drop-shadow-xl base-bg-light dark:bg-black lg:col-start-7 lg:col-end-11 lg:row-start-3 lg:row-end-4">
       <el-text class="text-xl">{{ $t("files") }}</el-text>
       <el-upload v-model:file-list="form.files" list-type="picture-card">
         <template #file="{ file }">
@@ -106,12 +104,12 @@
           <Upload />
         </el-icon>
       </el-upload>
-    </div>
+    </el-card>
 
     <!-- comments -->
-    <div class="mt-4 p-4 drop-shadow-xl bg-white dark:bg-black dark:border-[#4c4d4f] dark:border-[1px] rounded-[4px]">
-      <el-input v-model="newComment" type="textarea" resize="vertical" :rows="2" :placeholder='$t("newComment")' />
-      <el-button class="float-end mt-2" type="primary" size="small">{{ $t("sendComment") }}</el-button>
+    <el-card class="drop-shadow-xl base-bg-light dark:bg-black self-start lg:col-start-1 lg:col-end-7 lg:row-start-3 lg:row-end-5">
+      <el-input v-model="newComment" type="textarea" resize="vertical" :rows="3" :placeholder="$t('newComment')" />
+      <el-button class="mt-2 float-end" type="primary" size="small">{{ $t("sendComment") }}</el-button>
 
       <el-divider class="mt-10 mb-3" />
 
@@ -120,33 +118,25 @@
           <el-card class="block">
             <el-text size="large" tag="b" type="primary">dmeyer</el-text>
             <br />
-            <el-text size="default">Lorem ipsum dolor sit amet, **consetetur** sadipscing elitr, sed diam nonumy eirmod
-              tempor
-              invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
-              dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-              Lorem
-              ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-              dolore
-              magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-              clita
-              kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</el-text>
+            <el-text size="default"
+              >Lorem ipsum dolor sit amet, **consetetur** sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et
+              justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+              eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+              sanctus est Lorem ipsum dolor sit amet.</el-text
+            >
           </el-card>
         </el-timeline-item>
       </el-timeline>
-    </div>
+    </el-card>
 
     <!-- buttons -->
-    <div class="flex justify-between base-bg-light dark:base-bg-dark">
-      <el-button class="text-sm drop-shadow-xl" type="default" :icon="Printer">{{
-        $t("pdfExport") }}</el-button>
+    <div class="flex justify-between lg:col-span-full lg:row-start-5 lg:row-end-6">
+      <el-button class="text-sm drop-shadow-xl" type="default" :icon="Printer">{{ $t("pdfExport") }}</el-button>
 
       <div class="flex">
-        <el-button class="text-sm justify-self-end drop-shadow-xl" type="primary">{{
-          $t("save") }}</el-button>
+        <el-button class="text-sm justify-self-end drop-shadow-xl" type="primary">{{ $t("save") }}</el-button>
 
-        <el-button class="text-sm justify-self-end drop-shadow-xl" type="default">{{
-          $t("close") }}</el-button>
-
+        <el-button class="text-sm justify-self-end drop-shadow-xl" type="default">{{ $t("close") }}</el-button>
       </div>
     </div>
   </div>
@@ -154,11 +144,11 @@
 
 <script lang="ts" setup>
 import { ArrowLeft, EditPen, Upload, ZoomIn, Download, Delete, Printer } from "@element-plus/icons-vue";
-import { contrastColor } from 'contrast-color';
+import { contrastColor } from "contrast-color";
 import type state from "~/types/state";
 import type priority from "~/types/priority";
 import type building from "~/types/building";
-import type { UploadProps, UploadUserFile } from 'element-plus'
+import type { UploadProps, UploadUserFile } from "element-plus";
 
 const router = useRouter();
 
@@ -174,7 +164,7 @@ let form = ref({
   createdAt: undefined as string | undefined,
   author: undefined as string | undefined,
   description: undefined as string | undefined,
-  files: userFiles as UploadUserFile[]
+  files: userFiles as UploadUserFile[],
 });
 </script>
 
@@ -201,26 +191,11 @@ function determineViewWidth() {
 }
 
 const stateOptions: state[] = [
-  {
-    id: "1",
-    name: "new",
-    color: "#E71D36",
-  },
-  {
-    id: "2",
-    name: "open",
-    color: "#1B998B",
-  },
-  {
-    id: "3",
-    name: "closed",
-    color: "#C5D86D",
-  },
-  {
-    id: "4",
-    name: "Emilio ist doof",
-    color: "#2E294E",
-  },
+  ...Array.from({ length: 100 }).map((_, i) => ({
+    id: (i + 1).toString(),
+    name: `option ${i + 1}`,
+    color: `hsl(${(i * 12) % 360}, 100%, 50%)`,
+  })),
 ];
 
 const priorityOptions: priority[] = [
@@ -259,12 +234,16 @@ const buildingOptions: building[] = [
 
 const userFiles: UploadUserFile[] = [
   {
-    name: "ThisIsAFileName.txt",
-    url: "https://i.pinimg.com/474x/03/8a/bf/038abf64c89b9cb7f522e59843f5eb92.jpg"
+    name: "angry link",
+    url: "https://i.pinimg.com/474x/03/8a/bf/038abf64c89b9cb7f522e59843f5eb92.jpg",
   },
   {
     name: "Bored Link",
     url: "https://static1.cbrimages.com/wordpress/wp-content/uploads/2023/11/img_0183.jpeg",
-  }
-]
+  },
+  {
+    name: `pissed link`,
+    url: "https://pm1.aminoapps.com/6913/821d0c4190c8e3e9599b056f4e6d1649598bb5bbr1-1600-1200v2_hq.jpg",
+  },
+];
 </script>

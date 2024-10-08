ae192e28081f6b60c24aa791f9a186c7934a9ca5
Dustdev_DM <public@dustin-dm.de>
2024-10-08 19:21:57 +0200

    add missing api repositories

--- frontend/plugins/api.ts
+++ frontend/plugins/api.ts
@@ -2,12 +2,16 @@ import TicketRepository from "~/repositories/ticket";
 import StateRepository from "~/repositories/state";
 import PriorityRepository from "~/repositories/priority";
 import BuildingRepository from "~/repositories/building";
+import CommentRepository from "~/repositories/comment";
+import AttachmentRepository from "~/repositories/attachment";
 
 interface IApiInstance {
   ticket: TicketRepository;
   state: StateRepository;
   priority: PriorityRepository;
   building: BuildingRepository;
+  comment: CommentRepository;
+  attachment: AttachmentRepository;
 }
 
 export default defineNuxtPlugin(nuxtApp => {
@@ -16,6 +20,8 @@ export default defineNuxtPlugin(nuxtApp => {
     state: new StateRepository(),
     priority: new PriorityRepository(),
     building: new BuildingRepository(),
+    comment: new CommentRepository(),
+    attachment: new AttachmentRepository(),
   };
 
   return {

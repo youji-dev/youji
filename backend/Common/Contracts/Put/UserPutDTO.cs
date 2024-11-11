6b2d10b89c7b665d742eb4a999f13827d8ab2625
yst_ <erik.leimert@web.de>
2024-11-11 16:15:11 +0100

    updated roleassignement controller to conform to expended user entity

---
+++ backend/Common/Contracts/Put/UserPutDTO.cs
@@ -0,0 +1,13 @@
+﻿using Common.Enums;
+
+namespace Common.Contracts.Put
+{
+    /// <summary>
+    /// DTO for update operations on users
+    /// </summary>
+    /// <param name="NewRole">A new role assigement; set to null to leave unchanged</param>
+    public record class UserPutDTO(
+        Roles? NewRole)
+    {
+    }
+}
--- backend/WebApi/Controllers/RoleAssignmentController.cs
+++ backend/WebApi/Controllers/RoleAssignmentController.cs
@@ -1,6 +1,6 @@
 ﻿using Application.WebApi.Decorators;
+using Common.Contracts.Put;
 using Common.Enums;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using PersistenceLayer.DataAccess.Entities;
 using PersistenceLayer.DataAccess.Repositories;
@@ -18,51 +18,76 @@ namespace Application.WebApi.Controllers
         /// <summary>
         /// Gets all role assignments.
         /// </summary>
-        /// <param name="roleAssignmentRepo">Instance of <see cref="RoleAssignmentRepository"/></param>
-        /// <returns>An <see cref="ObjectResult"/> with all role assignments.</returns>
+        /// <param name="userRepository">Instance of <see cref="UserRepository"/></param>
+        /// <returns>An <see cref="ObjectResult"/> with all users.</returns>
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public ActionResult<RoleAssignment[]> Get(
-            [FromServices] RoleAssignmentRepository roleAssignmentRepo)
+        public ActionResult<User[]> Get(
+            [FromServices] UserRepository userRepository)
         {
-            return this.Ok(roleAssignmentRepo.GetAll().ToArray());
+            return this.Ok(userRepository.GetAll().ToArray());
         }
 
         /// <summary>
         /// Adds a new role assignment entity.
         /// </summary>
-        /// <param name="roleAssignmentRepo">Instance of <see cref="RoleAssignmentRepository"/></param>
-        /// <param name="roleAssignment">The <see cref="RoleAssignment"/> that will be added.</param>
-        /// <returns>An <see cref="ObjectResult"/> with the added role assignment entity.</returns>
+        /// <param name="roleAssignmentRepo">Instance of <see cref="UserRepository"/></param>
+        /// <param name="roleAssignment">The <see cref="User"/> that should be added.</param>
+        /// <returns>An <see cref="ObjectResult"/> with the new user entity.</returns>
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<ActionResult<RoleAssignment>> Post(
-            [FromServices] RoleAssignmentRepository roleAssignmentRepo,
-            [FromBody] RoleAssignment roleAssignment)
+        public async Task<ActionResult<User>> Post(
+            [FromServices] UserRepository roleAssignmentRepo,
+            [FromBody] User roleAssignment)
         {
             await roleAssignmentRepo.AddAsync(roleAssignment);
             return this.Ok(roleAssignment);
         }
 
         /// <summary>
-        /// Updates the specific role assignment.
+        /// Update a user
         /// </summary>
-        /// <param name="roleAssignmentRepo">Instance of <see cref="RoleAssignmentRepository"/></param>
-        /// <param name="userId">The user id of the role assignment.</param>
-        /// <returns>An <see cref="ObjectResult"/> with the updated role assignment.</returns>
-        [HttpDelete("{roleAssignmentId}")]
+        /// <param name="userRepository">Instance of <see cref="UserRepository"/></param>
+        /// <param name="userId">The id of the user to update</param>
+        /// <param name="userUpdate">An update </param>
+        /// <returns>The updated user</returns>
+        [HttpPut("{userId}")]
+        public async Task<ActionResult<User>> UpdateRole(
+            [FromServices] UserRepository userRepository,
+            [FromRoute] string userId,
+            [FromBody] UserPutDTO userUpdate)
+        {
+            User? user = await userRepository.GetAsync(userId);
+            if (user is null)
+                return this.NotFound($"No user found for id '{userId}'");
+
+            if (userUpdate.NewRole is not null)
+                user.Type = (Roles)userUpdate.NewRole;
+
+            await userRepository.UpdateAsync(user);
+
+            return this.Ok(user);
+        }
+
+        /// <summary>
+        /// Deletes a user
+        /// </summary>
+        /// <param name="userRepository">Instance of <see cref="UserRepository"/></param>
+        /// <param name="userId">The id of the user.</param>
+        /// <returns>The result of the action with descriptive text.</returns>
+        [HttpDelete("{userId}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<string>> Delete(
-            [FromServices] RoleAssignmentRepository roleAssignmentRepo,
+            [FromServices] UserRepository userRepository,
             [FromRoute] string userId)
         {
-            var deleteRoleAssignment = await roleAssignmentRepo.GetAsync(userId);
+            var deleteRoleAssignment = await userRepository.GetAsync(userId);
 
             if (deleteRoleAssignment is null)
                 return this.NotFound($"A role assignment with de id '{userId}' doesn´t exist.");
 
-            await roleAssignmentRepo.DeleteAsync(deleteRoleAssignment);
+            await userRepository.DeleteAsync(deleteRoleAssignment);
 
             return this.Ok($"The role assignment with the id '{userId}' was deleted.");
         }

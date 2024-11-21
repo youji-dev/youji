bd59cc49a0b5daec35619746f2b32f98258985d1
Dustin <private@dustin-dm.de>
2024-11-21 10:58:55 +0100

    add abiltiy to preview image attachments, downalad and delete attachments

--- frontend/components/ticketDetail/ticketFiles.vue
+++ frontend/components/ticketDetail/ticketFiles.vue
@@ -1,9 +1,10 @@
 <template>
-  <el-card class="drop-shadow-xl base-bg-light dark:base-bg-dark lg:min-h-[13.9rem]">
+  <el-card :v-loading="loading" class="drop-shadow-xl base-bg-light dark:base-bg-dark lg:min-h-[13.9rem]">
     <el-text class="text-xl">{{ $t("files") }}</el-text>
     <el-upload v-model:file-list="ticket.attachments" list-type="picture-card">
       <template #file="{ file }">
-        <UnLazyImage v-if="file.blurHash" class="object-cover aspect-square w-full" :src="$api.attachment.generateAttachmentURL(file.id)" :blurhash="file.blurHash" :lazy-load="true" />
+        <UnLazyImage v-if="file.blurHash" class="object-cover aspect-square w-full"
+          :src="$api.attachment.generateAttachmentURL(file.id)" :blurhash="file.blurHash" :lazy-load="true" />
 
         <div v-else class="justify-center align-center text-center w-full h-full flex flex-col">
           <FileIcons class="self-center" :width="30" :height="30" :name="file.name" />
@@ -12,15 +13,15 @@
 
         <div>
           <span class="el-upload-list__item-actions">
-            <span class="el-upload-list__item-preview" @click="">
+            <span v-if="file.blurHash" class="el-upload-list__item-preview" @click="openPreview(file)">
               <el-icon><zoom-in /></el-icon>
             </span>
-            <span class="el-upload-list__item-delete" @click="">
+            <span class="el-upload-list__item-delete" @click="downloadFile(file)">
               <el-icon>
                 <Download />
               </el-icon>
             </span>
-            <span class="el-upload-list__item-delete" @click="">
+            <span class="el-upload-list__item-delete" @click="deleteFile(file)">
               <el-icon>
                 <Delete />
               </el-icon>
@@ -33,16 +34,97 @@
       </el-icon>
     </el-upload>
   </el-card>
+  <el-dialog v-model="dialogVisible">
+    <img w-full :src="dialogImageUrl" alt="Preview Image" class="w-full" />
+  </el-dialog>
 </template>
 
 <script lang="ts" setup>
 import type ticket from "~/types/api/response/ticketResponse";
 import { Upload, ZoomIn, Download, Delete } from "@element-plus/icons-vue";
 import FileIcons from "file-icons-vue";
+import type ticketAttachment from "~/types/api/response/ticketAttachmentResponse";
 
 const { $api } = useNuxtApp();
+const i18n = useI18n();
 
-defineProps<{
+const props = defineProps<{
   ticket: ticket;
 }>();
+
+let dialogVisible = ref(false);
+let dialogImageUrl = ref("");
+let loading = ref(false);
+
+
+function openPreview(file: ticketAttachment) {
+  dialogImageUrl.value = $api.attachment.generateAttachmentURL(file.id);
+  dialogVisible.value = true;
+}
+
+function downloadFile(file: ticketAttachment) {
+  const link = document.createElement('a');
+  link.href = $api.attachment.generateAttachmentURL(file.id);
+  link.setAttribute('download', file.name);
+  link.style.display = 'none';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+async function deleteFile(file: ticketAttachment) {
+  loading.value = true;
+  try {
+    var attachmentDeleteResult = await $api.attachment.delete(file.id);
+
+    if (attachmentDeleteResult.error.value) {
+      if (attachmentDeleteResult.error.value.statusCode === 403) {
+        throw new Error(i18n.t("forbidden"));
+      }
+      if (attachmentDeleteResult.error.value.statusCode === 500) {
+        throw new Error("serverError");
+      }
+      if (attachmentDeleteResult.error.value.message) {
+        throw new Error(attachmentDeleteResult.error.value.message);
+      }
+      if (attachmentDeleteResult.error.value.data) {
+        throw new Error(attachmentDeleteResult.error.value.data);
+      } else {
+        throw new Error(i18n.t("error"));
+      }
+    }
+
+    var attachmentFetchResult = await $api.ticket.getAttachments(props.ticket.id)
+
+    if (attachmentFetchResult.error.value) {
+      if (attachmentFetchResult.error.value.statusCode === 403) {
+        throw new Error(i18n.t("forbidden"));
+      }
+      if (attachmentFetchResult.error.value.statusCode === 500) {
+        throw new Error("serverError");
+      }
+      if (attachmentFetchResult.error.value.message) {
+        throw new Error(attachmentFetchResult.error.value.message);
+      }
+      if (attachmentFetchResult.error.value.data) {
+        throw new Error(attachmentFetchResult.error.value.data);
+      } else {
+        throw new Error(i18n.t("error"));
+      }
+    }
+
+    props.ticket.attachments = attachmentFetchResult.data.value ?? [];
+  } catch (error) {
+    ElNotification({
+      title: i18n.t("error"),
+      message: (error as Error).message,
+      type: "error",
+      duration: 5000,
+    });
+  }
+  finally {
+    loading.value = false;
+  }
+
+}
 </script>

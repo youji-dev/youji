2c938007b72752daa926dc59e60fee930111f5d7
DustdevDM <public@dustin-dm.de>
2025-01-13 17:42:32 +0100

    add dropdown select

--- frontend/components/ticketDetail/ticketDropdown.vue
+++ frontend/components/ticketDetail/ticketDropdown.vue
@@ -2,16 +2,33 @@
   <div class="grid lg:grid-cols-2 lg:grid-rows-4 gap-1">
     <div>
       <el-text>{{ $t("state") }}</el-text>
-      <el-select v-model="ticket.state" value-key="id" class="drop-shadow-xl dark:base-bg-dark"
-        :placeholder="$t('select')">
-        <el-option v-for="state in availableStates" :key="state.id" :label="state.name" :value="state">
+      <el-select
+        v-model="ticket.state"
+        value-key="id"
+        class="drop-shadow-xl dark:base-bg-dark"
+        :placeholder="$t('select')"
+      >
+        <el-option
+          v-for="state in availableStates"
+          :key="state.id"
+          :label="state.name"
+          :value="state"
+        >
           <el-tag :color="state.color">
-            <el-text :style="{ color: contrastColor({ bgColor: state.color }) }"> {{ state.name }}</el-text>
+            <el-text
+              :style="{ color: contrastColor({ bgColor: state.color }) }"
+            >
+              {{ state.name }}</el-text
+            >
           </el-tag>
         </el-option>
         <template #label>
           <div class="flex items-center">
-            <el-tag :color="ticket.state.color" size="small" class="mr-2 aspect-square" />
+            <el-tag
+              :color="ticket.state.color"
+              size="small"
+              class="mr-2 aspect-square"
+            />
             <span class="truncate">{{ ticket.state.name }}</span>
           </div>
         </template>
@@ -19,26 +36,75 @@
     </div>
     <div>
       <el-text>{{ $t("priority") }}</el-text>
-      <el-select v-model="ticket.priority" value-key="value" class="drop-shadow-xl dark:base-bg-dark"
+      <!-- <el-select v-model="ticket.priority" value-key="value" class="drop-shadow-xl dark:base-bg-dark"
         :placeholder="$t('select')">
         <el-option v-for="priority in availablePriorities" :key="priority.id" :label="priority.name"
           :value="priority" />
+      </el-select> -->
+      <el-select
+        v-model="ticket.priority"
+        value-key="id"
+        class="drop-shadow-xl dark:base-bg-dark"
+        :placeholder="$t('select')"
+      >
+        <el-option
+          v-for="priority in availablePriorities"
+          :key="priority.id"
+          :label="priority.name"
+          :value="priority"
+        >
+          <el-tag :color="priority.color">
+            <el-text
+              :style="{ color: contrastColor({ bgColor: priority.color }) }"
+            >
+              {{ priority.name }}</el-text
+            >
+          </el-tag>
+        </el-option>
+        <template #label>
+          <div class="flex items-center">
+            <el-tag
+              :color="ticket.priority.color"
+              size="small"
+              class="mr-2 aspect-square"
+            />
+            <span class="truncate">{{ ticket.priority.name }}</span>
+          </div>
+        </template>
       </el-select>
     </div>
     <div class="lg:col-span-full">
       <el-text>{{ $t("building") }}</el-text>
-      <el-select v-model="ticket.building" class="drop-shadow-xl dark:base-bg-dark" value-key="id" :clearable="true"
-        :placeholder="$t('select')">
-        <el-option v-for="building in availableBuildings" :key="building.id" :label="building.name" :value="building" />
+      <el-select
+        v-model="ticket.building"
+        class="drop-shadow-xl dark:base-bg-dark"
+        value-key="id"
+        :clearable="true"
+        :placeholder="$t('select')"
+      >
+        <el-option
+          v-for="building in availableBuildings"
+          :key="building.id"
+          :label="building.name"
+          :value="building"
+        />
       </el-select>
     </div>
     <div class="lg:col-span-full">
       <el-text>{{ $t("room") }}</el-text>
-      <el-input v-model="ticket.room" class="drop-shadow-xl dark:base-bg-dark" :placeholder="$t('enter')" />
+      <el-input
+        v-model="ticket.room"
+        class="drop-shadow-xl dark:base-bg-dark"
+        :placeholder="$t('enter')"
+      />
     </div>
     <div class="lg:col-span-full">
       <el-text>{{ $t("object") }}</el-text>
-      <el-input v-model="ticket.object" class="drop-shadow-xl dark:base-bg-dark" :placeholder="$t('enter')" />
+      <el-input
+        v-model="ticket.object"
+        class="drop-shadow-xl dark:base-bg-dark"
+        :placeholder="$t('enter')"
+      />
     </div>
   </div>
 </template>
@@ -62,10 +128,22 @@ let availableBuildings: Ref<building[]> = ref([]);
 
 onNuxtReady(async () => {
   try {
-    const [states, priorities, buildings] = await Promise.all([$api.state.getAll(), $api.priority.getAll(), $api.building.getAll()]);
+    const statesPromise = $api.state.getAll();
+    const prioritiesPromise = $api.priority.getAll();
+    const buildingsPromise = $api.building.getAll();
+
+    const [states, prioritiesResponse, buildings] = await Promise.all([
+      statesPromise,
+      prioritiesPromise,
+      buildingsPromise,
+    ]);
+
+    const sortedPriorities = prioritiesResponse.data.value?.sort(
+      (a, b) => b.value - a.value
+    );
 
     availableStates.value = states.data.value ?? [];
-    availablePriorities.value = priorities.data.value ?? [];
+    availablePriorities.value = sortedPriorities ?? [];
     availableBuildings.value = buildings.data.value ?? [];
   } catch (error) {
     ElNotification({

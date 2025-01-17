0aa3ec3dc0d67d56e1092ce75000d98190f57640
Erik <erik.leimert@web.de>
2025-01-17 12:30:48 +0100

    add IsRenderableImage property to attachments

---
+++ backend/DataAccessLayer/Migrations/20250117111214_Add_IsRenderableImage_To_TicketAttachment.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersistenceLayer.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_IsRenderableImage_To_TicketAttachment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRenderableImage",
+                table: "Attachments",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRenderableImage",
+                table: "Attachments");
+        }
+    }
+}

521e1c9176aa023e16d395d7129b21af704d2914
DustdevDM <public@dustin-dm.de>
2025-02-25 19:35:57 +0100

    fix correct hadling of null values for mail authentication values

--- backend/BusinessLogic/Mailing/MailingService.cs
+++ backend/BusinessLogic/Mailing/MailingService.cs
@@ -18,8 +18,8 @@ namespace DomainLayer.BusinessLogic.Mailing
         private readonly string mailSenderAddress = configuration.GetValueOrThrow("SenderAddress", ["Mail"]);
         private readonly string mailServerAddress = configuration.GetValueOrThrow("SmtpAddress", ["Mail"]);
         private readonly int mailServerPort = int.Parse(configuration.GetValueOrThrow("SmtpPort", ["Mail"]));
-        private readonly string? mailServerAuthenticationUsername = configuration.GetValueOrThrow("SmtpUser", ["Mail"]);
-        private readonly string? mailServerAuthenticationPassword = configuration.GetValueOrThrow("SmtpPassword", ["Mail"]);
+        private readonly string mailServerAuthenticationUsername = configuration.GetValueOrThrow("SmtpUser", ["Mail"]);
+        private readonly string mailServerAuthenticationPassword = configuration.GetValueOrThrow("SmtpPassword", ["Mail"]);
         private readonly bool useSsl = bool.Parse(configuration.GetValueOrThrow("UseSsl", ["Mail"]));
         private readonly CompositeFormat mailSubjectFormat = CompositeFormat.Parse(
             configuration.GetValueOrThrow("SubjectFormat", ["Mail"]));
@@ -39,7 +39,7 @@ namespace DomainLayer.BusinessLogic.Mailing
             using SmtpClient client = new();
             await client.ConnectAsync(this.mailServerAddress, this.mailServerPort, this.useSsl);
 
-            if (this.mailServerAuthenticationUsername is not null || this.mailServerAuthenticationPassword is not null)
+            if (this.mailServerAuthenticationUsername != string.Empty || this.mailServerAuthenticationPassword != string.Empty)
             {
                 await client.AuthenticateAsync(
                     this.mailServerAuthenticationUsername,

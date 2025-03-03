923c6ed6e095d7d63828835889dc6dc2986aae33
Emilio Imscher <emilio.irmscher@hsc-solutions.eu>
2025-03-03 10:32:06 +0100

    Squashed commit of the following:
    commit 2b85e68726ee0e87319b31a28b7a4e6ced47c517
    Author: DustdevDM <public@dustin-dm.de>
    Date:   Wed Feb 26 17:50:21 2025 +0100
    
        Change User Patch to allow non Admins to change their own NewPreferredEmailLcid
    
    commit 1d7b0535145715e29fca54ba2d8ca19625a2fc0e
    Author: DustdevDM <public@dustin-dm.de>
    Date:   Wed Feb 26 17:39:14 2025 +0100
    
        Change User Get endpoint to be usable by non admins but only return the own entity when done so

--- backend/Common/Contracts/Patch/UserPatch.cs
+++ backend/Common/Contracts/Patch/UserPatch.cs
@@ -6,7 +6,7 @@ namespace Common.Contracts.Patch
     /// DTO for update operations on users
     /// </summary>
     /// <param name="NewRole">A new role assignment; set to null to leave unchanged</param>
-    /// <param name="NewPreferredLcid">A new preffered language code; set to null to leave unchanged</param>
+    /// <param name="NewPreferredEmailLcid">A new preferred language code; set to null to leave unchanged</param>
     public record class UserPatch(
         Roles? NewRole,
         string? NewPreferredEmailLcid)
